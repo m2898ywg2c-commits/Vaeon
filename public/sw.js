60054d11fe89e9fd238edbcc61496bf5874b30ef
James <m2898ywg2c@privaterelay.appleid.com>
2026-07-18 15:06:42 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "workout-tracker-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
